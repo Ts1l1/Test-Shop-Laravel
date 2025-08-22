a993dde387634985994815e50b71557754fd44cb
Ts1l1 <tsikaridze.li@mail.ru>
2025-08-23 04:41:23 +0500

    Migrations, Modules, Admin

---
+++ src/app/Models/Category.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($model) {
+            if (empty($model->slug)) {
+                $model->slug = Str::slug($model->name);
+            }
+        });
+    }
+
+    // Если хочешь связи — например, одна категория может иметь продукты
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
